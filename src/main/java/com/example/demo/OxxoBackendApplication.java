35231b08155dbe9e41abfbac2373adc76338988e
MichelGalindo <michelpogbamodric1019@gmail.com>
2021-05-03 20:08:45 -0500

    Commit Inicial

---
+++ src/main/java/com/example/demo/OxxoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxxoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxxoBackendApplication.class, args);
+	}
+
+}
